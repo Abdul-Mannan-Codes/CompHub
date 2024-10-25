9fe47bbec365d0e84cbd3d82563269402eaf6cd3
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-10-26 01:37:18 +0530

    Get Started Section created

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ function loadContent(){
         else
             image.style.backgroundImage = ` linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1)), url("images/comp${i}.jpg")`;
     }
+    if(localStorage.getItem('username')==null){
+        display('#get-started');
+    }
+    else{
+        hide('#get-started');
+    }
+}
+function display(element){
+    document.querySelector(element).style.display = "block";
+}
+function hide(element){
+    document.querySelector(element).style.display = "none";
 }
