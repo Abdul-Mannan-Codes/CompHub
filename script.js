9b3630925bb6456d08119b7e4db1092d4220417d
Abdul-Mannan-Codes <sdabmannan786@gmail.com>
2024-11-04 21:35:22 +0530

    More sections added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var flag=true, n=0,timer1,timer2,timer3;
+var acc = document.querySelector('#account-count');
+var res = document.querySelector('#resource-count');
+var proj = document.querySelector('#project-count');
 function loadContent(){
     let image;
     for(let i=1;i<7;i++){
@@ -9,15 +13,49 @@ function loadContent(){
             image.style.backgroundImage = ` linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1)), url("images/comp${i}.jpg")`;
     }
     if(localStorage.getItem('username')==null){
-        display('#get-started');
+        display('#create-account');
     }
     else{
-        hide('#get-started');
+        hide('#create-account');
     }
 }
 function display(element){
-    document.querySelector(element).style.display = "block";
+    document.querySelector(element).style.display = "";
 }
 function hide(element){
     document.querySelector(element).style.display = "none";
+}
+function startCounter(){
+    if(flag){
+        timer1=setInterval(incAccount,10);
+        timer2=setInterval(incResource,10);
+        timer3=setInterval(incProject,10);
+        flag=false;
+    }
+}
+function incAccount(){
+    acc.textContent = n;
+    if(n==100){
+        clearInterval(timer1);
+        acc.textContent += '+';
+        n=0;
+    }
+    n++;
+}
+function incResource(){
+    res.textContent = n;
+    if(n==250){
+        clearInterval(timer2);
+        res.textContent += '+';
+        n=0;
+    }
+    n++;
+}
+function incProject(){
+    proj.textContent = n;
+    if(n==25){
+        clearInterval(timer3);
+        proj.textContent += '+';
+    }
+    n++;
 }
